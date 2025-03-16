c0ca2cdf45bfec2a13ab2ce99ffa4dfe848bcce1
AdvaitRegeGD <advaitrege.business@gmail.com>
2025-03-16 17:59:19 +0530

    ScriptAdding

--- Assets/CollegeStudent/Animations/MC_movement_script.cs
+++ Assets/CollegeStudent/Animations/MC_movement_script.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class MC_movement_script : MonoBehaviour
 {
+
+    public animator am;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,9 @@ public class MC_movement_script : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(keyCode.D))
+        {
+            
+        }
     }
 }
